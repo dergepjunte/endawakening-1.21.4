d7e4b4730d188e03c3295b6eb778daee38fe402b
DerGepjunte <junus.bajbouj@icloud.com>
2025-06-06 17:20:53 +0200

    Initial commit

---
+++ src/main/java/ja/endawakening/EndAwakening.java
@@ -0,0 +1,16 @@
+package ja.endawakening;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EndAwakening implements ModInitializer {
+	public static final String MOD_ID = "endawakening";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/ja/endawakening/EndAwakeningClient.java
@@ -0,0 +1,10 @@
+package ja.endawakening;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EndAwakeningClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
