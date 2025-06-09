c986e3453bc20bc2bc69d3baa63cce3ce7685937
dergepjunte <junus.bajbouj@icloud.com>
2025-06-09 16:11:47 +0200

    First Block added

--- src/main/java/ja/endawakening/EndAwakening.java
+++ src/main/java/ja/endawakening/EndAwakening.java
@@ -1,16 +1,21 @@
 package ja.endawakening;
 
+import ja.endawakening.block.ModBlocks;
 import net.fabricmc.api.ModInitializer;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class EndAwakening implements ModInitializer {
+
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final String MOD_ID = "endawakening";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-
+		ModBlocks.initialize();
+		LOGGER.info("Hello Fabric world!");
 	}
 }
